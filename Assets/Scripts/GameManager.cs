212f7d691c342249ab1e4668c102973363329ff2
dbaric99 <damjana.baric1999@gmail.com>
2024-06-08 18:57:45 +0200

    added scene management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public int World { get; private set; }
+    public int Stage { get; private set; }
+    public int Lives { get; private set; }
 
     private void Awake()
     {
@@ -25,5 +27,24 @@ public class GameManager : MonoBehaviour
         {
             Instance = null;
         }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        Lives = 3;
+        LoadLevel(1, 1);
+    }
+
+    private void LoadLevel(int world, int stage)
+    {
+        World = world;
+        Stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}");
     }
 }
