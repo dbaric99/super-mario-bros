6c94338d30fa7a5d26ae8c872fc52faf3d364ca5
dbaric99 <damjana.baric1999@gmail.com>
2024-06-09 12:31:04 +0200

    setup for entity movements

---
+++ Assets/Scripts/EntityMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class EntityMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector2 direction = Vector2.left;
+
+    private new Rigidbody2D rigidbody;
+    private Vector2 velocity;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        enabled = false;
+    }
+
+    private void OnBecomeVisible()
+    {
+        enabled = true;
+    }
+
+    private void OnBecomeInvisible()
+    {
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        rigidbody.WakeUp();
+    }
+
+    private void OnDisable()
+    {
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.Sleep();
+    }
+}
