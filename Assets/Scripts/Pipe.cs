fb90a34e44f5eebf5bfdc5ce71a2c7ee7f9edf33
dbaric99 <damjana.baric1999@gmail.com>
2024-06-09 23:07:31 +0200

    added pipe exit

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -29,6 +29,22 @@ public class Pipe : MonoBehaviour
         Vector3 enteredScale = Vector3.one * 0.5f;
 
         yield return Move(player, enteredPosition, enteredScale);
+        yield return new WaitForSeconds(1f);
+
+        Camera.main.GetComponent<CameraController>().SetUndergroud(connection.position.y < 0f);
+
+        if (exitDirection != Vector3.zero)
+        {
+            player.position = connection.position - exitDirection;
+            yield return Move(player, connection.position + exitDirection, Vector3.one);
+        }
+        else
+        {
+            player.position = connection.position;
+            player.localScale = Vector3.one;
+        }
+
+        player.GetComponent<PlayerMovement>().enabled = true;
     }
 
     private IEnumerator Move(Transform player, Vector3 endPosition, Vector3 endScale)
