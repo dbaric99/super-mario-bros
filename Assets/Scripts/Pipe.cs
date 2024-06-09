e60b167bd45c9c286ec5b0ef6335fb056c98ea18
dbaric99 <damjana.baric1999@gmail.com>
2024-06-09 22:48:49 +0200

    added pipe enter player animation

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    public KeyCode enterKeyCode = KeyCode.S;
+    public Vector3 enterDirection = Vector3.down;
+    public Vector3 exitDirection = Vector3.zero;
+    public Transform connection;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (connection != null && other.CompareTag(PLAYER_TAG))
+        {
+            if (Input.GetKey(enterKeyCode))
+            {
+                StartCoroutine(Enter(other.transform));
+            }
+        }
+    }
+
+    private IEnumerator Enter(Transform player)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+
+        Vector3 enteredPosition = transform.position + enterDirection;
+        Vector3 enteredScale = Vector3.one * 0.5f;
+
+        yield return Move(player, enteredPosition, enteredScale);
+    }
+
+    private IEnumerator Move(Transform player, Vector3 endPosition, Vector3 endScale)
+    {
+        float elapsed = 0f;
+        float duration = 1f;
+
+        Vector3 startPosition = player.position;
+        Vector3 startScale = player.localScale;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+
+            player.position = Vector3.Lerp(startPosition, endPosition, t);
+            player.localScale = Vector3.Lerp(startScale, endScale, t);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        player.position = endPosition;
+        player.localScale = endScale;
+    }
+}
