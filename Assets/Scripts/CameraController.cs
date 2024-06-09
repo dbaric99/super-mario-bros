5a7a00e08ce4abb5165f95d40abbf1a3659023c9
dbaric99 <damjana.baric1999@gmail.com>
2024-06-09 23:09:51 +0200

    added camera movement for underground section

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
+
+    public float height = 7.04f;
+    public float undergroundHeight = -7.01f;
+
     private Transform player;
 
     private void Awake()
@@ -14,6 +18,13 @@ public class CameraController : MonoBehaviour
     {
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+
+    public void SetUndergroud(bool isUnderground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = isUnderground ? undergroundHeight : height;
         transform.position = cameraPosition;
     }
 }
